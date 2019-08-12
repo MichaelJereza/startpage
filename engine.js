f6e9621b77f14de3208c60a4e80b681c5b9a65cc
MichaelJereza <mdj1999@gmail.com>
2019-08-11 20:15:08 -0700

    Google, Duck, Ecosia, Qwant, Yandex

--- engine.js
+++ engine.js
@@ -1,17 +1,22 @@
 var s_engine=[
-  "Google",
-  "DuckDuckGo",
-  "Ecosia",
-  "Quant",
-  "Yandex",
-  "Startpage"
+  "https://www.google.com/search?q=^",
+  "https://duckduckgo.com/?q=^",
+  "https://www.ecosia.org/search?q=^",
+  "https://www.qwant.com/?q=^",
+  "https://yandex.com/search/?text=^"
 ];
-var searches=document.querySelectorAll('.Search_Box');
+window.onload=function(){
 
-searches.foreach(function(){
-  for(var i=0;i<s_engine.size;i++){
-    if(s_engine[i]===this.placeholder){
-      alert(s_engine[i]);
+
+  var searches=document.querySelectorAll('.Search_Box');
+  var button=document.querySelector('#Search_Button');
+  console.log(searches);
+  button.addEventListener("click", function(){
+    for(var s=0;s<searches.length;s++){
+      if(searches[s].value){
+        var url=s_engine[s].replace('^', searches[s].value);
+        window.location.assign(url);
+      }
     }
-  }
-})
+  })
+}
